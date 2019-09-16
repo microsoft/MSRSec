dc0aa4db880a542518a0605a2267df48ba284891
Daniel McIlvaney <damcilva@microsoft.com>
2019-09-16 14:40:41 -0700

    Expand TODO comment

--- TAs/optee_ta/AuthVars/src/varmgmt.c
+++ TAs/optee_ta/AuthVars/src/varmgmt.c
@@ -215,10 +215,20 @@ AuthVarInitStorage(
             status = InitializeDefaultVariables();
             if (status != TEE_SUCCESS) {
                 EMSG("Failed to set default variables");
-                // TODO: We should set up all the variables which were just
-                // created, or add variable by variable checking to make sure
-                // we don't miss any variables in the case one fails, or the 
-                // TA is interuppted during its first boot.
+                // TODO: Make this operation atomic.
+
+                // Ex: Setting default variables {A, B, C, db, kek, pk}.
+                // If {A, B, C} are set correctly, but setting db fails, the TA
+                // we be in a bad state. The TA will be 'initialized' since it
+                // has at least one persistent object available (A, B, and C),
+                // and will make no further attempts to set db, kek, pk.
+                //
+                // This step must be made atomic either by removing the variables,
+                // or setting a NV flag when done. Setting a flag is probably
+                // preferable since it will handle both graceful failures and
+                // premature power cycles.
+
+                // WipeMemory();
                 goto Cleanup;
             }
 #endif
